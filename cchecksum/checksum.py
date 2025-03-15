d459e7dd5a06e81aae509123b63a54e6c6d0c914
BobTheBuidler <70677534+BobTheBuidler@users.noreply.github.com>
2025-03-15 16:42:07 -0400

    feat: optimize `eth_utils.to_hex` (#5)
    * feat: optimize `eth_utils.to_hex`
    
    * chore: `black .`
    
    * chore: remove unused import
    
    * fix: mis
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- cchecksum/checksum.py
+++ cchecksum/checksum.py
@@ -1,9 +1,9 @@
-import binascii
-from typing import Union
+from binascii import hexlify
+from typing import Optional, Union
 
 from eth_hash.auto import keccak
-from eth_typing import AnyAddress, ChecksumAddress, HexAddress
-from eth_utils import hexstr_if_str, to_hex
+from eth_typing import AnyAddress, ChecksumAddress, HexAddress, HexStr, Primitives
+from eth_utils import add_0x_prefix, encode_hex, hexstr_if_str
 from eth_utils.address import _HEX_ADDRESS_REGEXP
 
 from cchecksum._checksum import cchecksum
@@ -41,7 +41,7 @@ def to_checksum_address(value: Union[AnyAddress, str, bytes]) -> ChecksumAddress
     """
     norm_address_no_0x = to_normalized_address(value)[2:]
     address_hash = bytes(keccak(norm_address_no_0x.encode("utf-8")))
-    address_hash_hex_no_0x = binascii.hexlify(address_hash).decode("ascii")
+    address_hash_hex_no_0x = hexlify(address_hash).decode("ascii")
     return cchecksum(norm_address_no_0x, address_hash_hex_no_0x)
 
 
@@ -51,12 +51,12 @@ def to_normalized_address(value: Union[AnyAddress, str, bytes]) -> HexAddress:
 
     This function ensures that the address is in a consistent lowercase hexadecimal
     format, which is useful for further processing or validation. It uses
-    :func:`eth_utils.hexstr_if_str` and :func:`eth_utils.to_hex` to convert the input
+    :func:`eth_utils.hexstr_if_str` and :func:`to_hex` to convert the input
     to a hexadecimal string.
 
     Args:
         value: The address to be normalized. It can be in any format supported by
-            :func:`eth_utils.to_hex`.
+            :func:`to_hex`.
 
     Raises:
         ValueError: If the input address is not in a recognized format.
@@ -110,3 +110,30 @@ def is_address(value: str) -> bool:
         - :func:`eth_utils.is_address` for the standard implementation.
     """
     return _HEX_ADDRESS_REGEXP.fullmatch(value) is not None
+
+
+BytesLike = Union[Primitives, bytearray, memoryview]
+
+
+def to_hex(
+    address_bytes: Optional[BytesLike] = None,
+    *,
+    hexstr: Optional[HexStr] = None,
+) -> HexStr:
+    """
+    Auto converts any supported value into its hex representation.
+    Trims leading zeros, as defined in:
+    https://github.com/ethereum/wiki/wiki/JSON-RPC#hex-value-encoding
+    """
+    if hexstr is not None:
+        return add_0x_prefix(hexstr.lower())
+
+    if isinstance(address_bytes, (bytes, bytearray)):
+        return encode_hex(address_bytes)
+
+    if isinstance(address_bytes, memoryview):
+        return encode_hex(bytes(address_bytes))
+
+    raise TypeError(
+        f"Unsupported type: '{repr(type(address_bytes))}'. Must be one of: bytes or bytearray."
+    )
