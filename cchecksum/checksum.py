2d3cc3abc864ef17b2de5fa7a9b96b78038d1235
BobTheBuidler <70677534+BobTheBuidler@users.noreply.github.com>
2025-04-17 14:54:57 -0400

    fix: replace lower()

--- cchecksum/checksum.py
+++ cchecksum/checksum.py
@@ -101,12 +101,12 @@ def to_normalized_address_no_0x(value: Union[AnyAddress, str, bytes]) -> HexAddr
 
     # if `hex_address_no_0x` is not a valid address
     if hex_address_fullmatch(f"0x{hex_address_no_0x}") is None:
-        hex_address = f"0x{hex_address_no_0x}"
+        hex_address = f"0x{hex_address_no_0x}".lower()
         raise ValueError(
             f"Unknown format {repr(value)}, attempted to normalize to {repr(hex_address)}"
         )
 
-    return hex_address_no_0x  # type: ignore [return-value]
+    return hex_address_no_0x.lower()  # type: ignore [return-value]
 
 
 del AnyStr, Optional, Union
