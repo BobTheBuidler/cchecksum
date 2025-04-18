8ccab0b22d6a77cb590fb88102bf9ae90d028b89
BobTheBuidler <70677534+BobTheBuidler@users.noreply.github.com>
2025-04-18 09:49:43 -0400

    chore: move to_normalized_address_no_0x to cython file (#41)

--- cchecksum/checksum.py
+++ cchecksum/checksum.py
@@ -1,22 +1,17 @@
-import re
 from binascii import hexlify
-from typing import AnyStr, Optional, Union
+from typing import Union
 
 from eth_hash.auto import keccak
-from eth_typing import AnyAddress, ChecksumAddress, HexAddress, HexStr
-from eth_utils.hexadecimal import _HEX_REGEXP
+from eth_typing import AnyAddress, ChecksumAddress
 from eth_utils.toolz import compose
 
-from cchecksum._checksum import cchecksum
+from cchecksum._checksum import cchecksum, to_normalized_address_no_0x
 
 
 # force _hasher_first_run and _preimage_first_run to execute so we can cache the new hasher
 keccak(b"")
 
 hash_address = compose(hexlify, bytes, keccak.hasher, str.encode)
-
-hex_address_fullmatch = re.compile("[0-9a-f]{40}", re.ASCII).fullmatch
-hex_fullmatch = _HEX_REGEXP.fullmatch
 
 
 # this was ripped out of eth_utils and optimized a little bit
@@ -53,55 +48,6 @@ def to_checksum_address(value: Union[AnyAddress, str, bytes]) -> ChecksumAddress
     return cchecksum(norm_address_no_0x, hash_address(norm_address_no_0x))
 
 
-def to_normalized_address_no_0x(value: Union[AnyAddress, str, bytes]) -> HexAddress:
-    """
-    Converts an address to its normalized hexadecimal representation without the '0x' prefix.
-
-    This function ensures that the address is in a consistent lowercase hexadecimal
-    format, which is useful for further processing or validation.
-
-    Args:
-        value: The address to be normalized.
-
-    Raises:
-        ValueError: If the input address is not in a recognized format.
-        TypeError: If the input is not a string, bytes, or any address type.
-
-    Examples:
-        >>> to_normalized_address("0xB47E3CD837DDF8E4C57F05D70AB865DE6E193BBB")
-        '0xb47e3cd837ddf8e4c57f05d70ab865de6e193bbb'
-
-        >>> to_normalized_address(b'\xb4~<\xd87\xdd\xf8\xe4\xc5\x7f\x05\xd7\n\xb8e\xden\x19;\xbb')
-        '0xb47e3cd837ddf8e4c57f05d70ab865de6e193bbb'
-
-    See Also:
-        - :func:`eth_utils.to_normalized_address` for the standard implementation.
-    """
-    if isinstance(value, str):
-        hex_address_no_0x = (value[2:] if value.startswith(("0x", "0X")) else value).lower()
-
-        # if `value` has content and is not a hexstring
-        if hex_address_no_0x and hex_fullmatch(value) is None:
-            raise ValueError("when sending a str, it must be a hex string. " f"Got: {repr(value)}")
-
-    elif isinstance(value, (bytes, bytearray)):
-        hex_address_no_0x = hexlify(value).decode("ascii").lower()
-
-    else:
-        raise TypeError(
-            f"Unsupported type: '{repr(type(value))}'. Must be one of: bool, str, bytes, bytearray or int."
-        )
-
-    # if `hex_address_no_0x` is not a valid address
-    if hex_address_fullmatch(hex_address_no_0x) is None:
-        hex_address = f"0x{hex_address_no_0x}"
-        raise ValueError(
-            f"Unknown format {repr(value)}, attempted to normalize to {repr(hex_address)}"
-        )
-
-    return hex_address_no_0x  # type: ignore [return-value]
-
-
-del AnyStr, Optional, Union
-del AnyAddress, ChecksumAddress, HexAddress, HexStr
-del _HEX_REGEXP, compose, keccak
+del Union
+del AnyAddress, ChecksumAddress
+del compose, keccak
