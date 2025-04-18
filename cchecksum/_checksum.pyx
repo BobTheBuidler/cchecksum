4779c9671ab312f60519255971b56919e6757043
BobTheBuidler <70677534+BobTheBuidler@users.noreply.github.com>
2025-04-18 12:12:35 -0400

    Update _checksum.pyx

--- cchecksum/_checksum.pyx
+++ cchecksum/_checksum.pyx
@@ -122,45 +122,42 @@ cdef inline void validate_hex_address(unicode hex_address_no_0x, object original
 
     
 cdef inline void validate_hex_chars(unicode string, object original_value):
+    # NOTE: `string` should already be lowercase when passed in
     cdef char c
     for c in string:
-        validate_hex_char(c)
-
-        
-cdef inline void validate_hex_char(char c):
-    if c == 48:  # 0
-        pass
-    elif c == 49:  # 1
-        pass
-    elif c == 50:  # 2
-        pass
-    elif c == 51:  # 3
-        pass
-    elif c == 52:  # 4
-        pass
-    elif c == 53:  # 5
-        pass
-    elif c == 54:  # 6
-        pass
-    elif c == 55:  # 7
-        pass
-    elif c == 56:  # 8
-        pass
-    elif c == 57:  # 9
-        pass
-    elif c == 97:  # a
-        pass
-    elif c == 98:  # b
-        pass
-    elif c == 99:  # c
-        pass
-    elif c == 100:  # d
-        pass
-    elif c == 101:  # e
-        pass
-    elif c == 102:  # f
-        pass
-    else:
-        raise ValueError(
-            f"Unknown format {repr(original_value)}, attempted to normalize to "0x{string}"
-        )
+        if c == 48:  # 0
+            pass
+        elif c == 49:  # 1
+            pass
+        elif c == 50:  # 2
+            pass
+        elif c == 51:  # 3
+            pass
+        elif c == 52:  # 4
+            pass
+        elif c == 53:  # 5
+            pass
+        elif c == 54:  # 6
+            pass
+        elif c == 55:  # 7
+            pass
+        elif c == 56:  # 8
+            pass
+        elif c == 57:  # 9
+            pass
+        elif c == 97:  # a
+            pass
+        elif c == 98:  # b
+            pass
+        elif c == 99:  # c
+            pass
+        elif c == 100:  # d
+            pass
+        elif c == 101:  # e
+            pass
+        elif c == 102:  # f
+            pass
+        else:
+            raise ValueError(
+                f"Unknown format {repr(original_value)}, attempted to normalize to "0x{string}"
+            )
