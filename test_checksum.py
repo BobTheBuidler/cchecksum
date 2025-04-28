6116d096025462a6c73946de82e731cbc3df0396
BobTheBuidler <70677534+BobTheBuidler@users.noreply.github.com>
2025-04-28 03:41:02 -0400

    feat(test): test threadsafety

--- test_checksum.py
+++ test_checksum.py
@@ -7,22 +7,22 @@ import pytest
 from cchecksum import to_checksum_address
 
 
-def test_checksum_str():
+def test_checksum_str() -> None:
     lower = "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2".lower()
     assert to_checksum_address(lower) == "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2"
 
 
-def test_checksum_str_no_0x_prefix():
+def test_checksum_str_no_0x_prefix() -> None:
     lower = "C02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2".lower()
     assert to_checksum_address(lower) == "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2"
 
 
-def test_checksum_bytes():
+def test_checksum_bytes() -> None:
     bytes = unhexlify("C02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2")
     assert to_checksum_address(bytes) == "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2"
 
 
-def test_type_error():
+def test_type_error() -> None:
     input = object()
     try:
         eth_utils.to_checksum_address(input)
@@ -33,7 +33,7 @@ def test_type_error():
         raise RuntimeError("this should not happen")
 
 
-def test_int_type_error():
+def test_int_type_error() -> None:
     # this gives a ValueError in eth_utils but that's weird
     with pytest.raises(
         TypeError,
@@ -42,7 +42,7 @@ def test_int_type_error():
         to_checksum_address(0)
 
 
-def test_none_type_error():
+def test_none_type_error() -> None:
     # this gives a ValueError in eth_utils but that's due to an implementation detail of hexstr_to_str
     with pytest.raises(
         TypeError,
@@ -51,7 +51,7 @@ def test_none_type_error():
         to_checksum_address(None)
 
 
-def test_value_error():
+def test_value_error() -> None:
     input = "i am a bad string"
     try:
         eth_utils.to_checksum_address(input)
@@ -67,11 +67,16 @@ benchmark_addresses = []
 range_start = 100000000000000000000000000000000000000000
 for i in range(range_start, range_start + 500000):
     address = hex(i)[2:]
-    address = "0x" + "0" * (40 - len(address)) + address
+    address = f"0x{'0' * (40 - len(address))}{address}"
     benchmark_addresses.append(address)
 
+def test_threadsafety() -> None:
+    test_addresses = benchmark_addresses[:1000]
+    map = ThreadPoolExecutor.map
+    cchecksum = map(to_checksum_address, test_addresses)
+    eth_utils = map(eth_utils.to_checksum_address, test_addresses)
 
-def test_benchmark():
+def test_benchmark() -> None:
     start = time.time()
     checksummed = list(map(to_checksum_address, benchmark_addresses))
     cython_duration = time.time() - start
