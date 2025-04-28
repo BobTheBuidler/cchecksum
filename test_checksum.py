b5f93d464a879496c2c55361a0fa8b06757ee41c
BobTheBuidler <70677534+BobTheBuidler@users.noreply.github.com>
2025-04-28 04:08:41 -0400

    feat(test): test threadsafety (#85)
    * feat(test): test threadsafety
    
    * chore: `black .`
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- test_checksum.py
+++ test_checksum.py
@@ -1,5 +1,7 @@
 import time
 from binascii import unhexlify
+from concurrent.futures import ThreadPoolExecutor
+from functools import lru_cache
 
 import eth_utils
 import pytest
@@ -7,22 +9,37 @@ import pytest
 from cchecksum import to_checksum_address
 
 
-def test_checksum_str():
+benchmark_addresses = []
+range_start = 100000000000000000000000000000000000000000
+
+
+@lru_cache(maxsize=None)
+def __get_prefix(addrlen: int) -> str:
+    return f"0x{'0' * (40 - len(address))}"
+
+
+for i in range(range_start, range_start + 500000):
+    address = hex(i)[2:]
+    address = f"{__get_prefix(len(address))}{address}"
+    benchmark_addresses.append(address)
+
+
+def test_checksum_str() -> None:
     lower = "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2".lower()
     assert to_checksum_address(lower) == "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2"
 
 
-def test_checksum_str_no_0x_prefix():
+def test_checksum_str_no_0x_prefix() -> None:
     lower = "C02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2".lower()
     assert to_checksum_address(lower) == "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2"
 
 
-def test_checksum_bytes():
+def test_checksum_bytes() -> None:
     bytes = unhexlify("C02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2")
     assert to_checksum_address(bytes) == "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2"
 
 
-def test_type_error():
+def test_type_error() -> None:
     input = object()
     try:
         eth_utils.to_checksum_address(input)
@@ -33,7 +50,7 @@ def test_type_error():
         raise RuntimeError("this should not happen")
 
 
-def test_int_type_error():
+def test_int_type_error() -> None:
     # this gives a ValueError in eth_utils but that's weird
     with pytest.raises(
         TypeError,
@@ -42,7 +59,7 @@ def test_int_type_error():
         to_checksum_address(0)
 
 
-def test_none_type_error():
+def test_none_type_error() -> None:
     # this gives a ValueError in eth_utils but that's due to an implementation detail of hexstr_to_str
     with pytest.raises(
         TypeError,
@@ -51,7 +68,7 @@ def test_none_type_error():
         to_checksum_address(None)
 
 
-def test_value_error():
+def test_value_error() -> None:
     input = "i am a bad string"
     try:
         eth_utils.to_checksum_address(input)
@@ -62,16 +79,7 @@ def test_value_error():
         raise RuntimeError("this should not happen")
 
 
-# Benchmark
-benchmark_addresses = []
-range_start = 100000000000000000000000000000000000000000
-for i in range(range_start, range_start + 500000):
-    address = hex(i)[2:]
-    address = "0x" + "0" * (40 - len(address)) + address
-    benchmark_addresses.append(address)
-
-
-def test_benchmark():
+def test_benchmark() -> None:
     start = time.time()
     checksummed = list(map(to_checksum_address, benchmark_addresses))
     cython_duration = time.time() - start
@@ -83,3 +91,11 @@ def test_benchmark():
     assert checksummed == python
     assert cython_duration < python_duration
     print(f"took {cython_duration/python_duration}% of the time")
+
+
+def test_threadsafety() -> None:
+    test_addresses = benchmark_addresses[:1000]
+    executor = ThreadPoolExecutor(100)
+    fast_way = executor.map(to_checksum_address, test_addresses)
+    slow_way = executor.map(eth_utils.to_checksum_address, test_addresses)
+    assert list(fast_way) == list(slow_way)
