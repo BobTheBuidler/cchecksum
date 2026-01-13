26f3efc00200db43716a73004875924a0a1c1701
BobTheBuidler <70677534+BobTheBuidler@users.noreply.github.com>
2026-01-13 14:35:56 -0400

    chore: bump version to 0.3.9

--- setup.py
+++ setup.py
@@ -1,17 +1,6 @@
-import os
 from pathlib import Path
 from Cython.Build import cythonize
 from setuptools import find_packages, setup
-
-# -----------------------------------------------------------------------------
-# Always ignore SETUPTOOLS_SCM_PRETEND_VERSION for cchecksum builds.
-# This environment variable is sometimes set by downstream projects at build time.
-# It can cause cchecksum to be built with the wrong version metadata, leading to
-# install failures. We never use this env var for cchecksum, so it is safe for us
-# to remove it from the cchecksum build environment.
-# -----------------------------------------------------------------------------
-if "SETUPTOOLS_SCM_PRETEND_VERSION" in os.environ:
-    del os.environ["SETUPTOOLS_SCM_PRETEND_VERSION"]
 
 with open("requirements.txt", "r") as f:
     requirements = list(map(str.strip, f.read().split("\n")))[:-1]
@@ -22,7 +11,7 @@ long_description = (this_directory / "README.md").read_text()
 setup(
     name="cchecksum",
     packages=find_packages(),
-    version="0.3.8",
+    version="0.3.9",
     description="A ~8x faster drop-in replacement for eth_utils.to_checksum_address. Raises the exact same Exceptions. Implemented in C.",
     long_description=long_description,
     long_description_content_type="text/markdown",
