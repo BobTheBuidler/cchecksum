d973b348508790bab06fad70a98f8fc3b00e672a
BobTheBuidler <70677534+BobTheBuidler@users.noreply.github.com>
2026-01-18 17:17:27 -0400

    fix(benchmark): increase iterations for better consistency (#143)
    * fix(benchmark): increase iterations for better consistency
    
    * Update pytest.yaml
    
    * Delete benchmarks/batch.py
    
    * Update test_checksum_benchmarks.py
    
    * Update test_checksum_benchmarks.py

--- benchmarks/test_checksum_benchmarks.py
+++ benchmarks/test_checksum_benchmarks.py
@@ -1,9 +1,8 @@
-from typing import Union
+from typing import Any, Callable, Union
 
 import pytest
 from pytest_codspeed import BenchmarkFixture
 
-from benchmarks.batch import batch
 from benchmarks.data import (
     BYTEARRAY_CASES,
     BYTEARRAY_CASE_IDS,
@@ -14,19 +13,22 @@ from benchmarks.data import (
 )
 from cchecksum import to_checksum_address
 
-BATCH_SIZE = 1000
+
+def _run_10k(func: Callable[..., Any], *args: Any, **kwargs: Any) -> None:
+    for _ in range(10_000):
+        func(*args, **kwargs)
 
 
 @pytest.mark.benchmark(group="to_checksum_address_str")
 @pytest.mark.parametrize("value", STR_CASES, ids=STR_CASE_IDS)
 def test_to_checksum_address_str(benchmark: BenchmarkFixture, value: str) -> None:
-    benchmark(batch, BATCH_SIZE, to_checksum_address, value)
+    benchmark(_run_10k, to_checksum_address, value)
 
 
 @pytest.mark.benchmark(group="to_checksum_address_bytes")
 @pytest.mark.parametrize("value", BYTES_CASES, ids=BYTES_CASE_IDS)
 def test_to_checksum_address_bytes(benchmark: BenchmarkFixture, value: bytes) -> None:
-    benchmark(batch, BATCH_SIZE, to_checksum_address, value)
+    benchmark(_run_10k, to_checksum_address, value)
 
 
 @pytest.mark.benchmark(group="to_checksum_address_bytearray")
@@ -34,4 +36,4 @@ def test_to_checksum_address_bytes(benchmark: BenchmarkFixture, value: bytes) ->
 def test_to_checksum_address_bytearray(
     benchmark: BenchmarkFixture, value: Union[bytes, bytearray]
 ) -> None:
-    benchmark(batch, BATCH_SIZE, to_checksum_address, value)
+    benchmark(_run_10k, to_checksum_address, value)
