6eb05c3bb21bf09ac8ebd5942562297283c1656f
BobTheBuidler <70677534+BobTheBuidler@users.noreply.github.com>
2026-01-18 16:49:38 -0400

    Delete benchmarks/batch.py

--- benchmarks/batch.py
+++
@@ -1,6 +0,0 @@
-from typing import Any, Callable
-
-
-def batch(iterations: int, func: Callable[..., Any], *args: Any, **kwargs: Any) -> None:
-    for _ in range(iterations):
-        func(*args, **kwargs)
