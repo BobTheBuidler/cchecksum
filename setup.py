e08c855a0d764a5502d18fb928ee101602ad943a
BobTheBuidler <70677534+BobTheBuidler@users.noreply.github.com>
2026-01-20 06:21:49 -0400

    chore: bump version to 0.3.10

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ long_description = (this_directory / "README.md").read_text()
 setup(
     name="cchecksum",
     packages=find_packages(),
-    version="0.3.9",
+    version="0.3.10",
     description="A ~8x faster drop-in replacement for eth_utils.to_checksum_address. Raises the exact same Exceptions. Implemented in C.",
     long_description=long_description,
     long_description_content_type="text/markdown",
