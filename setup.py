097de43b1c5b8503ef4cf8d5e7a39fed6f149ed3
BobTheBuidler <70677534+BobTheBuidler@users.noreply.github.com>
2026-01-20 21:37:00 -0400

    chore: drop support for python3.8

--- setup.py
+++ setup.py
@@ -2,11 +2,28 @@ from pathlib import Path
 from Cython.Build import cythonize
 from setuptools import Extension, find_packages, setup
 
+classifiers=[
+    "Intended Audience :: Developers",
+    "Programming Language :: Python :: 3",
+    "Programming Language :: Python :: 3.9",
+    "Programming Language :: Python :: 3.10",
+    "Programming Language :: Python :: 3.11",
+    "Programming Language :: Python :: 3.12",
+    "Programming Language :: Python :: 3.13",
+    "Programming Language :: Python :: 3.14",
+    "Programming Language :: Python :: Implementation :: CPython",
+    "Operating System :: OS Independent",
+    "Topic :: Software Development :: Libraries",
+]
+
 with open("requirements.txt", "r") as f:
     requirements = list(map(str.strip, f.read().split("\n")))[:-1]
 
 this_directory = Path(__file__).parent
 long_description = (this_directory / "README.md").read_text()
+
+sources = ["cchecksum/_checksum.pyx", "cchecksum/keccak.c"]
+extension = Extension("cchecksum._checksum", sources=sources, include_dirs=["cchecksum"])
 
 setup(
     name="cchecksum",
@@ -20,22 +37,10 @@ setup(
     url="https://github.com/BobTheBuidler/cchecksum",
     license="MIT",
     install_requires=requirements,
-    python_requires=">=3.8,<4",
+    python_requires=">=3.9,<4",
     package_data={"cchecksum": ["py.typed", "*.pxd", "**/*.pxd"]},
     include_package_data=True,
-    ext_modules=cythonize(
-        [
-            Extension(
-                "cchecksum._checksum",
-                sources=["cchecksum/_checksum.pyx", "cchecksum/keccak.c"],
-                include_dirs=["cchecksum"],
-            )
-        ],
-        compiler_directives={
-            "language_level": 3,
-            "embedsignature": True,
-            "linetrace": False,
-        },
-    ),
+    classifiers=classifiers,
+    ext_modules=cythonize([extension], compiler_directives={"language_level": 3, "embedsignature": True, "linetrace": False}),
     zip_safe=False,
 )
