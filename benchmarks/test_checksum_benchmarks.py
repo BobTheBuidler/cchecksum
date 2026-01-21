cd0e869adb21da6bf7b78c428b38603c3d3ec343
BobTheBuidler <70677534+BobTheBuidler@users.noreply.github.com>
2026-01-20 21:05:59 -0400

    feat: add container benchmarks for checksum throughput (#174)
    * feat: add container benchmarks to quantify bulk checksum throughput
    
    * chore: `black .`
    
    * Update data.py
    
    * Update test_checksum_benchmarks.py
    
    * chore: `black .`
    
    * Update test_checksum_benchmarks.py
    
    * Update test_checksum_benchmarks.py
    
    * Update data.py
    
    * chore: compile C files for source control (#176)
    
    Co-authored-by: BobTheBuidler <70677534+BobTheBuidler@users.noreply.github.com>
    
    * chore: `black .`
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- benchmarks/test_checksum_benchmarks.py
+++ benchmarks/test_checksum_benchmarks.py
@@ -1,39 +1,23 @@
-from typing import Any, Callable, Union
-
 import pytest
 from pytest_codspeed import BenchmarkFixture
 
-from benchmarks.data import (
-    BYTEARRAY_CASES,
-    BYTEARRAY_CASE_IDS,
-    BYTES_CASES,
-    BYTES_CASE_IDS,
-    STR_CASES,
-    STR_CASE_IDS,
-)
+from benchmarks.data import CONTAINER_CASES, CONTAINER_CASE_IDS, SINGLE_CASES, SINGLE_CASE_IDS
 from cchecksum import to_checksum_address
 
 
-def _run_10k(func: Callable[..., Any], *args: Any, **kwargs: Any) -> None:
-    for _ in range(10_000):
-        func(*args, **kwargs)
-
-
 @pytest.mark.benchmark(group="to_checksum_address_str")
-@pytest.mark.parametrize("value", STR_CASES, ids=STR_CASE_IDS)
-def test_to_checksum_address_str(benchmark: BenchmarkFixture, value: str) -> None:
-    benchmark(_run_10k, to_checksum_address, value)
+@pytest.mark.parametrize("value", SINGLE_CASES, ids=SINGLE_CASE_IDS)
+def test_to_checksum_address(benchmark: BenchmarkFixture, value: str) -> None:
+    @benchmark
+    def run_10k():
+        for _ in range(10_000):
+            to_checksum_address(value)
 
 
-@pytest.mark.benchmark(group="to_checksum_address_bytes")
-@pytest.mark.parametrize("value", BYTES_CASES, ids=BYTES_CASE_IDS)
-def test_to_checksum_address_bytes(benchmark: BenchmarkFixture, value: bytes) -> None:
-    benchmark(_run_10k, to_checksum_address, value)
-
-
-@pytest.mark.benchmark(group="to_checksum_address_bytearray")
-@pytest.mark.parametrize("value", BYTEARRAY_CASES, ids=BYTEARRAY_CASE_IDS)
-def test_to_checksum_address_bytearray(
-    benchmark: BenchmarkFixture, value: Union[bytes, bytearray]
-) -> None:
-    benchmark(_run_10k, to_checksum_address, value)
+@pytest.mark.benchmark(group="to_checksum_address_bytes_container")
+@pytest.mark.parametrize("values", CONTAINER_CASES, ids=CONTAINER_CASE_IDS)
+def test_to_checksum_address_multi(benchmark: BenchmarkFixture, values: list[bytes]) -> None:
+    @benchmark
+    def run_container():
+        for value in values:
+            to_checksum_address(value)
