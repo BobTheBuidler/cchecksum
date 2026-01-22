9ae8ff7700344dc009eb92fed09b4268ff08a370
BobTheBuidler <70677534+BobTheBuidler@users.noreply.github.com>
2026-01-21 23:50:08 -0400

    fix(test): avoid web3 install failures on Windows runners for Python 3.13t and 3.14t (#206)
    * fix: gate web3 on Windows 3.13+ to avoid install failures
    
    * Update requirements-dev.txt
    
    * Update test_monkey_patch.py

--- tests/test_monkey_patch.py
+++ tests/test_monkey_patch.py
@@ -1,8 +1,14 @@
 import eth_utils
 import eth_utils.address
+import pytest
+
+# pywin32 does not install properly on free-threaded python builds on Windows,
+# so we skip tests invoking web3.py for Python3.13+ on Windows.
+pytest.importorskip("web3")
+
+import web3._utils as web3_utils
 import web3.main as web3_main
 import web3.middleware as web3_middleware
-import web3._utils as web3_utils
 
 import cchecksum
 
