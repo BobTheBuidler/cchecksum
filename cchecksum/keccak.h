c632206f8a80998ccf9f7ba1c4e2d10ce87115c1
BobTheBuidler <70677534+BobTheBuidler@users.noreply.github.com>
2026-01-28 01:32:02 -0400

    perf: add hex lookup tables to speed checksum normalization

--- cchecksum/keccak.h
+++ cchecksum/keccak.h
@@ -3,6 +3,9 @@
 
 #include <stddef.h>
 
+extern const unsigned char CKSUM_HEX_LOWER_MAP[256];
+extern const unsigned char CKSUM_HEX_UPPER_MAP[256];
+
 void keccak_256(const unsigned char* data, size_t len, unsigned char* out);
 
 #endif
